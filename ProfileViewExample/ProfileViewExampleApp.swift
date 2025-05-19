68c12aa11ea4ec407d83297a2627f1dbe9d51427
Rangerboy574 <jdeweese574@icloud.com>
2025-05-19 09:54:57 -0500

    Initial Commit

---
+++ ProfileViewExample/ProfileViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewExampleApp.swift
+//  ProfileViewExample
+//
+//  Created by Joseph DeWeese on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfileViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProfileViewExampleTests/ProfileViewExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewExampleTests.swift
+//  ProfileViewExampleTests
+//
+//  Created by Joseph DeWeese on 5/19/25.
+//
+
+import Testing
+@testable import ProfileViewExample
+
+struct ProfileViewExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
